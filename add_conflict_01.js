f5a573df8dfe8c49db20445b11a1099bfd7314f4
Jesse House <jesse.house@gmail.com>
2011-08-25 11:50:51 -0700

    adding conflict file after pulling in latest code

--- add_conflict_01.js
+++ add_conflict_01.js
@@ -1,2 +1,7 @@
+<<<<<<< HEAD
+// this file added from command line tool
+console.log("this is a test");
+=======
 // this file added from mac-github GUI app
 console.log("another test");
+>>>>>>> adding a file that already exists
